1acbb61179877634b5a14d341d5644b8c8506ae6
Jeremy Friemoth <jeremyfriemoth@gmail.com>
2025-11-08 16:19:53 -0500

    Simplify GitHub Actions workflow - use proven deployment method

--- test_ble.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_web_bluetooth/flutter_web_bluetooth.dart';
-
-void main() async {
-  // Test the API to see what's available
-  final bluetooth = FlutterWebBluetooth.instance;
-  
-  // This will show us the correct API
-  print('Bluetooth instance: $bluetooth');
-}
